ff75fa4f2f370f19d02fa49400e37eaeca036bfc
Adam Thornton <ademus4@gmail.com>
2017-04-15 19:56:36 +0100

    Database init script to fill dates table with 100 days from today

---
+++ init_db.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError, OperationalError
+from datetime import datetime, timedelta
+
+from models import Date
+from utils import db_connect, get_or_create
+
+Base = declarative_base()
+
+
+def main():
+    session = db_connect(Base)
+
+    # check to see if there are already 100 items in the date table
+    if session.query(Date).count() >= 100:
+        print("Error: database already contains 100 dates")
+        return
+
+    # otherwise initialise the db with 100 days from today
+    dates = [datetime.today() - timedelta(days=x) for x in range(0, 100)]
+    for item in dates:
+        date = {
+            'date': item.date(),
+            'day': item.day,
+            'month': item.month,
+            'year': item.year,
+            'day_of_week': item.weekday()
+        }
+        get_or_create(session, Date, **date)
+
+
+if __name__ == '__main__':
+    main()
