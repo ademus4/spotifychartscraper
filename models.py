a8dbdd3cc3ece6739cb9eecb8fc3dec069820b42
Adam Thornton <ademus4@gmail.com>
2017-04-16 17:06:45 +0100

    Added remaining models for the database structure along with relationships

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, DateTime, Integer
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 from utils import db_connect
@@ -15,6 +16,37 @@ class Date(Base):
     month = Column(Integer)
     year = Column(Integer)
     day_of_week = Column(Integer)
+    entries = relationship("Chart", backref="date")
+
+
+class Artist(Base):
+    __tablename__ = 'artist'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    tracks = relationship("Track", backref="artist")
+
+
+class Track(Base):
+    __tablename__ = 'track'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    url = Column(String(500))
+    entries = relationship("Chart", backref="track")
+    artist_id = Column(Integer, ForeignKey('artist.id'))
+
+
+class Chart(Base):
+    __tablename__ = 'chart'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    region = Column(String(250))
+    position = Column(Integer)
+    streams = Column(Integer)
+    date_id = Column(Integer, ForeignKey('date.id'))
+    track_id = Column(Integer, ForeignKey('track.id'))
 
 
 db_connect(Base)
