af5863b74d32a7729b94d7dd6ff219f1ba920c23
Adam Thornton <ademus4@gmail.com>
2017-04-17 23:08:48 +0100

    Added logging and refactored out the object saving to a seperate function

--- scrape_top200.py
+++ scrape_top200.py
@@ -1,6 +1,8 @@
 import requests
-import StringIO
 import csv
+import logging
+import sys
+import time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError, OperationalError
 from datetime import datetime, timedelta
@@ -11,6 +13,12 @@ from utils import db_connect, get_or_create
 Base = declarative_base()
 
 file_headers = ['Position', 'Track Name', 'Artist', 'Streams', 'URL']
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG,
+    stream=sys.stdout
+)
 
 
 def main():
@@ -24,35 +32,44 @@ def main():
 
         url_base = 'https://spotifycharts.com/regional/{}/daily/{}/download'
         url = url_base.format(region, date_str)
+        logging.info('URL: {}'.format(url))
 
-        r = requests.get(url)
+        try:
+            r = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            continue
         text_data = r.text.encode('ascii', 'ignore').splitlines()
         reader = csv.reader(text_data, delimiter=',')
         data = [line for line in reader]
         headers = data.pop(0)
         if headers == file_headers:
             for line in data:
-                position, track_name, artist_name, streams, url = line
-                artist = get_or_create(session, Artist, name=artist_name)
-
-                track_vals = {
-                    'title': track_name,
-                    'url': url,
-                    'artist': artist
-                }
-                track = get_or_create(session, Track, **track_vals)
-
-                chart_vals = {
-                    'type': 'top200',
-                    'region': region,
-                    'position': position,
-                    'streams': streams,
-                    'date': date,
-                    'track': track
-                }
-                chart = get_or_create(session, Chart, **chart_vals)
+                save_objects(session, line, region, date)
+            time.sleep(10)
         else:
-            print('Error with file! Incorrect headers')
+            logging.info('Error with file! Incorrect headers')
+
+
+def save_objects(session, data, region, date):
+    position, track_name, artist_name, streams, url = data
+    artist = get_or_create(session, Artist, name=artist_name)
+
+    track_vals = {
+        'title': track_name,
+        'url': url,
+        'artist': artist
+    }
+    track = get_or_create(session, Track, **track_vals)
+
+    chart_vals = {
+        'type': 'top200',
+        'region': region,
+        'position': position,
+        'streams': streams,
+        'date': date,
+        'track': track
+    }
+    chart = get_or_create(session, Chart, **chart_vals)
 
 if __name__ == '__main__':
     main()
