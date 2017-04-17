2f3fd3bc684424049e13fc44d66b797784c032c7
Adam Thornton <ademus4@gmail.com>
2017-04-17 21:44:16 +0100

    Iterate dates, request page and save the various objects to the database

--- scrape_top200.py
+++ scrape_top200.py
@@ -10,24 +10,49 @@ from utils import db_connect, get_or_create
 
 Base = declarative_base()
 
+file_headers = ['Position', 'Track Name', 'Artist', 'Streams', 'URL']
+
 
 def main():
     session = db_connect(Base)
 
     region = 'gb'
-    date = session.query(Date).first()
-    date_str = datetime.strftime(date.date, '%Y-%m-%d')
+    dates = session.query(Date).all()
 
-    url_base = 'https://spotifycharts.com/regional/{}/daily/{}/download'
-    url = url_base.format(region, date_str)
+    for date in dates:
+        date_str = datetime.strftime(date.date, '%Y-%m-%d')
 
-    r = requests.get(url)
-    f = StringIO.StringIO(r.text.encode('ascii', 'ignore'))
-    reader = csv.reader(f, delimiter=',')
-    data = [line for line in reader]
-    headers = data.pop(0)
-    import pdb; pdb.set_trace()
+        url_base = 'https://spotifycharts.com/regional/{}/daily/{}/download'
+        url = url_base.format(region, date_str)
 
+        r = requests.get(url)
+        text_data = r.text.encode('ascii', 'ignore').splitlines()
+        reader = csv.reader(text_data, delimiter=',')
+        data = [line for line in reader]
+        headers = data.pop(0)
+        if headers == file_headers:
+            for line in data:
+                position, track_name, artist_name, streams, url = line
+                artist = get_or_create(session, Artist, name=artist_name)
+
+                track_vals = {
+                    'title': track_name,
+                    'url': url,
+                    'artist': artist
+                }
+                track = get_or_create(session, Track, **track_vals)
+
+                chart_vals = {
+                    'type': 'top200',
+                    'region': region,
+                    'position': position,
+                    'streams': streams,
+                    'date': date,
+                    'track': track
+                }
+                chart = get_or_create(session, Chart, **chart_vals)
+        else:
+            print('Error with file! Incorrect headers')
 
 if __name__ == '__main__':
     main()
