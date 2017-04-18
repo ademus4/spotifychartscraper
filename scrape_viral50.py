4322e8319b3f3b74748edfa1cbd40cb96e6bc104
Adam Thornton <ademus4@gmail.com>
2017-04-18 21:48:57 +0100

    Created scraper for the viral50 page

---
+++ scrape_viral50.py
@@ -0,0 +1,80 @@
+import requests
+import csv
+import logging
+import sys
+import time
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError, OperationalError
+from datetime import datetime, timedelta
+
+from models import Date, Artist, Track, Chart, Link
+from utils import db_connect, get_or_create
+
+Base = declarative_base()
+
+file_headers = ['Position', 'Track Name', 'Artist', 'URL']
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG,
+    stream=sys.stdout
+)
+
+
+def main():
+    session = db_connect(Base)
+
+    region = 'gb'
+    dates = session.query(Date).all()
+
+    for date in dates:
+        date_str = datetime.strftime(date.date, '%Y-%m-%d')
+
+        url_base = 'https://spotifycharts.com/viral/{}/daily/{}/download'
+        url = url_base.format(region, date_str)
+        logging.info('URL: {}'.format(url))
+
+        try:
+            r = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            continue
+        text_data = r.text.encode('ascii', 'ignore').splitlines()
+        reader = csv.reader(text_data, delimiter=',')
+        data = [line for line in reader]
+        headers = data.pop(0)
+        if headers == file_headers:
+            for line in data:
+                save_objects(session, line, region, date)
+            time.sleep(10)
+        else:
+            logging.info('Error with file! Incorrect headers')
+
+
+def save_objects(session, data, region, date):
+    position, track_name, artist_name, url = data
+    artist = get_or_create(session, Artist, name=artist_name)
+
+    track_vals = {
+        'title': track_name,
+        'artist': artist
+    }
+    track = get_or_create(session, Track, **track_vals)
+
+    link_vals = {
+        'url': url,
+        'track': track
+    }
+    link = get_or_create(session, Link, **link_vals)
+
+    chart_vals = {
+        'type': 'viral50',
+        'region': region,
+        'position': position,
+        'streams': None,
+        'date': date,
+        'track': track
+    }
+    chart = get_or_create(session, Chart, **chart_vals)
+
+if __name__ == '__main__':
+    main()
